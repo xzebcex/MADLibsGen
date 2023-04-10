15b04d2c0ccbcb92a2cb1b65ec742170bded8a46
Aman Pandit <amanpandit3027@gmail.com>
2023-04-10 20:46:22 +0530

    Create mad_libs.py

---
+++ mad_libs.py
@@ -0,0 +1,34 @@
+# Mad Libs Generator
+
+
+# Questions for the user to answer
+
+noun = input('Choose a noun: ')
+
+p_noun = input('Choose a plural noun: ')
+
+noun2 = input('Choose a noun: ')
+
+place = input('Name a place: ')
+
+adjective = input('Choose an adjective (Describing word): ')
+
+noun3 = input('Choose a noun: ')
+
+# Print a story from the user input
+
+print('------------------------------------------')
+
+print(f'Be kind to your {noun} - footed {p_noun}')
+
+print(f'For a duck may be somebody\'s {noun2} ,')
+
+print(f'Be kind to your {p_noun} in place')
+
+print(f'Where the weather is always {adjective} . \n')
+
+print(f'You may think that is this the {noun3} ,')
+
+print('Well it is.')
+
+print('------------------------------------------')
